a915f9cb3ef16aa4f9f2e89fbc03fbeb138c0b99
Victor Gore <gorev2@mymacewan.ca>
2024-10-08 14:15:06 -0600

    Save local changes before merging

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import "./App.css";
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const App = () => {
-    return(
-        <div>App</div>
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>
+);
