9726afc3e354c864c2cc4d13eb21bb2d6a429f31
Victor Gore <64615476+victorgore@users.noreply.github.com>
2024-10-08 13:39:10 -0600

    Update index.js
    Fixed removed accidental line repeats from previous commit

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-import ReactDom from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-const root = ReactDom.createRoot(document.getElementById("root"));
-import ReactDom from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>
+);
