4809035f89270252c294d9481e0ce74a35e635a4
Victor Gore <gorev2@mymacewan.ca>
2024-10-08 13:47:47 -0600

    Added App.js file

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-import React from "react";
-import ReactDom from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import "./App.css";
 
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-  <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return(
+        <div>App</div>
